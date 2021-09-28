bd3a5c07f500be1927070b480b147940584ca099
phockett <phockett@users.noreply.github.com>
2021-09-28 11:03:29 -0400

    Updated author list.

--- automation/setAuthors.py
+++ automation/setAuthors.py
@@ -10,7 +10,7 @@ authorList = { 'Ben Sussman':{'ID':'IDU7HMMAAAAJ'},
             'Paul Hockett':{'ID':'e4FgTYMAAAAJ'},
             'Duncan England':{'ID':'IzqvkioAAAAJ'},
             'Phil Bustard':{'ID':None},
-            'Andrew Ridsdale':{'ID':None},
+            'Andrew Ridsdale':{'ID':'3RQEGVEAAAAJ'},
             'Khabat Heshami':{'ID':'fBBMAKwAAAAJ'},
           }
 
